480896820db0b2ce34d4ab168c62b17a015b0ec1
Antti Siirilä <antti.siirila@kone.com>
2017-10-18 22:08:37 +0300

    main suspended for 5s. Loop condition added.

--- src/main.c
+++ src/main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include "toggle_switch.h"
 #include <pthread.h>
+#include <unistd.h>
 
 int main(char argc, int argv[1])
 {
-pthread_mutex_t count_mutex;
- pthread_cond_t count_threshold_cv;
+    char count;
+    //pthread_mutex_t count_mutex;
+    //pthread_cond_t count_threshold_cv;
     c_toggle_switch lightSwitch;
 
     toggle_switch_init(&lightSwitch);
-for(;;)
-{
+    for(count=0;count < 10; count++)
+    {
 
 
-    printf("Light switch state %d [0 = off, 1 = on]\n", toggle_switch_is_on(&lightSwitch));
+        printf("Light switch state %d [0 = off, 1 = on]\n", toggle_switch_is_on(&lightSwitch));
 
-}
+        sleep(5);
+    }
 }
